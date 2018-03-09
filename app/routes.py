1a09312a1dc3f2978ac0c5ad7c0b5f53bd120f84
oxenprogrammer <emmy1000okello@gmail.com>
2018-03-09 20:46:12 +0300

    removed custom styling

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
