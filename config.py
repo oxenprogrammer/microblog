0554c2ac1ab6c434eb154bf82cfca19a2d717db4
oxenprogrammer <emmy1000okello@gmail.com>
2018-03-10 10:55:29 +0300

    added models

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'karungi is my love'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'sqlitedb.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
