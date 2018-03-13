f9830bded4969199ae181b0f450b14d17681db5e
oxenprogrammer <emmy1000okello@gmail.com>
2018-03-13 19:50:18 +0300

    adds mocked users

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,18 @@ from app.forms import LoginForm
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user = {'username':'emmisteel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
