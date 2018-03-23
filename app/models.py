d94c1a83f75f6fafe3966b2e6135a027550dabdf
oxenprogrammer <emmy1000okello@gmail.com>
2018-03-23 13:02:12 +0300

    adds follower post

--- app/models.py
+++ app/models.py
@@ -51,6 +51,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id
     ).count()>0
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id==Post.user_id).filter(
+                followers.c.follower_id==self.id
+            ).order_by(Post.timestamp.desc())
+    )
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
